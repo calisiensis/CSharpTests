33c5f18c79476fa6210cb833c4907719cf4eba79
Tomasz Schroeder <tomasz.schroeder@outlook.com>
2018-05-07 21:33:54 +0200

    Ostateczna wersja programiku

--- Powitanie/Program.cs
+++ Powitanie/Program.cs
@@ -10,7 +10,15 @@ namespace Powitanie
     {
         static void Main(string[] args)
         {
-            Console.Out.WriteLine("Pierwsze kroki w githubie!");
+            Console.Out.WriteLine("Pierwsze kroki w GitHubie!"); // zostało mi z Javy. Ten Out po Console nie jest potrzebny
+            Console.WriteLine("C# to nie Java, tu OUT nie jest potrzebny :)");
+            Console.WriteLine("");
+
+            foreach (var item in args) //Debugowanie -> właściwości -> elementy w tablicy imion
+            {
+                Console.WriteLine("Witaj, " + item);
+            }
+
         }
     }
 }
