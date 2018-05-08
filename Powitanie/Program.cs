d4977de7063a11b9d01f43971068cb3b3969ef1b
Tomasz Schroeder <tomasz.schroeder@outlook.com>
2018-05-08 20:10:00 +0200

    Wyodrębniono metody i koniec edycji programu

--- Powitanie/Program.cs
+++ Powitanie/Program.cs
@@ -10,19 +10,46 @@ namespace Powitanie
     {
         static void Main(string[] args)
         {
-            Console.Out.WriteLine("Pierwsze kroki w GitHubie!"); // zostało mi z Javy. Ten Out po Console nie jest potrzebny
-            Console.WriteLine("C# to nie Java, tu OUT nie jest potrzebny :)");
-            Console.WriteLine("");
+            Wstepniak();
 
             foreach (var item in args) //Debugowanie -> właściwości -> elementy w tablicy imion
             {
                 Console.WriteLine("Witaj, " + item);
             }
             for (; ; ) // pętla na nieskończone wykonywanie się programu
-            { 
-            Console.Write("\nWpisz swoje imię: "); //używamy \n zamiast kolejnej linijki kodu z Console.WriteLine();
-            string name = Console.ReadLine();
-            Console.WriteLine("Witaj, " + name);
+            {
+                Witajcie(); // refaktoryzacja metody ze zwykłego kodu
+
+                NapijmySieCzegos(); // jw
+
+                Ustawienia();
+
+            }
+        }
+
+        /// <summary> // dokumentacja w stylu XML
+        /// Uruchamiamy ekran startowy programu ;)
+        /// </summary>
+        private static void Wstepniak()
+        {
+            Console.Out.WriteLine("Pierwsze kroki w GitHubie!"); // zostało mi z Javy. Ten Out po Console nie jest potrzebny
+            Console.WriteLine("C# to nie Java, tu OUT nie jest potrzebny :)");
+            Console.WriteLine("");
+        }
+        /// <summary>
+        /// Robienie porządków na ekranie
+        /// </summary>
+        private static void Ustawienia()
+        {
+            Console.ReadKey(); // program nie zakończy się bez naciśnięcia jakiegoś klawisza
+            Console.Clear(); // czyszczenie ekranu
+            Console.ResetColor(); // przywraca pierwotne ustawienia koloru
+        }
+        /// <summary>
+        /// Sprawdzanie wieku i wyświetlanie komunikatów
+        /// </summary>
+        private static void NapijmySieCzegos()
+        {
             Console.Write("Wpisz ile masz lat: ");
             int wiek;
             bool result = int.TryParse(Console.ReadLine(), out wiek); //nie da się wpisać tu inta bezpośrednio, trzeba sparsować do inta - int.Parse, ale żeby zabezpieczyć się przed wpisaniem litery zamiast liczby, dajemy TryParse
@@ -42,11 +69,15 @@ namespace Powitanie
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("Pij mleko albo wodę z kałuży!");
             }
-                Console.ReadKey(); // program nie zakończy się bez naciśnięcia jakiegoś klawisza
-                Console.Clear(); // czyszczenie ekranu
-                Console.ResetColor(); // przywraca pierwotne ustawienia koloru
-
         }
+        /// <summary>
+        /// Powitanie użytkownika
+        /// </summary>
+        private static void Witajcie()
+        {
+            Console.Write("\nWpisz swoje imię: "); //używamy \n zamiast kolejnej linijki kodu z Console.WriteLine();
+            string name = Console.ReadLine();
+            Console.WriteLine("Witaj, " + name);
         }
     }
 }
