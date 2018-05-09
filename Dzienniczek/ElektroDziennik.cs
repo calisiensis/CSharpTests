3db1e6212c075baa3d159d691868d667dc8b197e
Tomasz Schroeder <tomasz.schroeder@outlook.com>
2018-05-09 21:51:09 +0200

    Finalna wersja programu Dzienniczek

---
+++ Dzienniczek/ElektroDziennik.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace Dzienniczek
+{
+    class ElektroDziennik
+    {
+
+        List<float> oceny = new List<float>();
+
+        public void DodawanieOcen(float ocena)
+        {
+            oceny.Add(ocena);
+        }
+
+        public float WyliczanieSredniej()
+        {
+            float suma = 0, srednia = 0;
+
+            foreach (var ocena in oceny) // zastąpiono rating zmienną ocena
+            {
+                suma += ocena;
+            }
+
+            srednia = suma / oceny.Count();
+
+            return srednia;
+        }
+
+        public float PokazMaxOcene()
+        {
+            return oceny.Max();
+        }
+
+        public float PokazMinOcene()
+        {
+            return oceny.Min();
+        }
+    }
+}
--- Dzienniczek/Program.cs
+++ Dzienniczek/Program.cs
@@ -10,7 +10,50 @@ namespace Dzienniczek
     {
         static void Main(string[] args)
         {
+            ElektroDziennik elektrodziennik = new ElektroDziennik();
 
+            for (; ; )
+            {
+                Console.Write("Podaj ocenę z zakresu 0.1 - 10 (liczba 69 powoduje wyjście z pętli): ");
+
+                float ocena;
+
+                bool result = float.TryParse(Console.ReadLine(), out ocena);
+
+                if(ocena == 69)
+                {
+                    break;
+                }
+                else if (ocena == 0)
+                {
+                    Console.WriteLine("Niepoprawna ocena, podaj liczbę z zakresu 0.1 - 10");
+                    continue;
+                }
+
+                if (result == true) // może być samo result
+                {
+                    if (ocena > 0 && ocena <= 10)
+                    {
+                        elektrodziennik.DodawanieOcen(ocena);
+                        continue;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Niepoprawna ocena, podaj liczbę z zakresu 0.1 - 10");
+                        continue;
+                    }
+
+                }
+
+
+                elektrodziennik.DodawanieOcen(ocena);
+            }
+
+            Console.WriteLine("Średnia Twoich ocen to: " + elektrodziennik.WyliczanieSredniej());
+            Console.WriteLine("Najwyższa ocena to: " + elektrodziennik.PokazMaxOcene());
+            Console.WriteLine("Najniższa ocena to: " + elektrodziennik.PokazMinOcene());
+            Console.ReadKey();
         }
     }
 }
