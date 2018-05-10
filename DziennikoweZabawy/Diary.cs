516d31a357835d9ff811fc133a36bf2c27478685
Tomasz Schroeder <tomasz.schroeder@outlook.com>
2018-05-10 14:11:55 +0200

    Testowanie wskaźników do obiektów

---
+++ DziennikoweZabawy/Diary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DziennikoweZabawy
+{
+    class Diary
+    {
+        public Diary()
+        {
+            oceny = new List<float>();
+        }
+        List<float> oceny; // = new List<float>(); to byśmy dali, gdyby nie konstruktor powyżej
+
+        public void DodawanieOcen(float ocena)
+        {
+            oceny.Add(ocena);
+        }
+
+        public float WyliczanieSredniej()
+        {
+            float suma = 0, srednia = 0;
+
+            foreach (var ocena in oceny) // zastąpiono rating zmienną ocena
+            {
+                suma += ocena;
+            }
+
+            srednia = suma / oceny.Count();
+
+            return srednia;
+        }
+
+        public float PokazMaxOcene()
+        {
+            return oceny.Max();
+        }
+
+        public float PokazMinOcene()
+        {
+            return oceny.Min();
+        }
+    }
+}
---
+++ DziennikoweZabawy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DziennikoweZabawy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary dzienniczek = new Diary(); // dzienniczek to zmienna referencyjna, bo przechowuje wskaźniki do obiektów
+            dzienniczek.DodawanieOcen(3);
+            dzienniczek.DodawanieOcen(5);
+            
+            Diary dzienniczek2 = dzienniczek; // wskazywanie na obiekt - będą przechowywać po 3 oceny
+            dzienniczek2.DodawanieOcen(8);
+
+            Diary dzienniczek3 = dzienniczek2; // combo wskaźnikowe
+            dzienniczek3.DodawanieOcen(1);
+        }
+    }
+}
