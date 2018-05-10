d31d2a1cd1913045d09778f8618dca4608852467
Tomasz Schroeder <tomasz.schroeder@outlook.com>
2018-05-10 13:20:03 +0200

    Mini program z tworzeniem konstruktorów

---
+++ Konstruktory/Osoba.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktory
+{
+    class Osoba
+    {
+        int id;
+        string firstName;
+        string lastName;
+        string address;
+        int age;
+        string gender;
+
+        public Osoba() // jak nie daliśmy wartości domyślnych, to będzie automatycznie 0, null lub false
+        {
+            id = 1;
+            firstName = "Emhyr";
+            lastName = "var Emreis";
+            age = 55;
+        }
+
+        public Osoba(int id, string firstName, string lastName, string address, int age, string gender)
+        {
+            this.id = id; // this - to id tyczy się naszej klasy/obiektu
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.age = age;
+            this.gender = gender;
+        }
+
+        public Osoba(int id, string firstName, string lastName)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            // można też dać dodatkowo konstruktory z domyślnymi wartościami
+            address = "Biały Sad";
+            age = 21;
+        }
+
+        public Osoba(int id, string firstName)
+        {
+            this.id = id;
+            this.firstName = firstName;
+        }
+    }
+
+
+}
---
+++ Konstruktory/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba osoba1 = new Osoba();
+            Osoba osoba2 = new Osoba(2, "Geralt");
+            Osoba osoba3 = new Osoba(3, "Letho", "z Gulety");
+            Osoba osoba4 = new Osoba(4, "Triss", "Merigold", "Novigrad", 33, "Kobieta");
+        }
+    }
+}
